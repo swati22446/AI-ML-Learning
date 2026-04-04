7dc9faa3164125b0569d8fd862fc3861dfb373b1
swati22446 <swati22446@gmail.com>
2026-04-04 23:40:17 +0530

    Array Creation

--- Data_Science_Essentials/day1_numpy_basics.py
+++ Data_Science_Essentials/day1_numpy_basics.py
@@ -1,4 +1,24 @@
 import numpy as np
 
+#CREATING AN ARRAY
 arr = np.array([1,2,3,4])
 print(arr)
+
+#Printing a zero matrix
+nums = np.zeros((3,2)) #3 rows and 0 columns
+print(nums)
+
+# print(np.zeros())  This will give error as the brackets are empty. The shape must be assigned
+
+#Printing ones
+print(np.ones((3,2)))
+
+#To print within a range --- (start, stop , step) --> stop value is not included & more precise for floats
+print(np.arange(1 ,10, 2))
+
+#Linespace array is one where we print non-whole numbers i.e. decimals --(start, stop , num) --> stop value is included
+
+print(np.linspace(0, 10 ,5)) 
+
+# MANIPULATING AN ARRAY
+
